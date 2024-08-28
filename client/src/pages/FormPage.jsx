19ba456e753e2708c55ae9947859dab38484d6d7
Mondeep Chetry <mandeepchetry6969@gmail.com>
2024-08-28 21:14:09 +0530

    #1 created the upload file form

--- client/src/pages/FormPage.jsx
+++ client/src/pages/FormPage.jsx
@@ -1,12 +1,14 @@
+import React from "react"
 import { Header } from "../components/Header"
-import { InputForm } from "../components/InputForm"
+
 import { Subheader } from "../components/Subheader"
+import InputForm from "../components/InputForm"
 
 export const FormPage = () => {
     return (
         <div className="bg-slate-300 h-screen flex justify-center">
-            <div className="flex flex-col justify-center">
-                <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+            <div className="flex flex-col justify-center w-full, max-w-4xl">
+                <div className="rounded-lg bg-white w-full text-center p-2 h-max px-4">
                     <Header label={"Upload a pdf"} />
                     <Subheader label={"Upload a pdf to find the most similar pdf"} />
                     <InputForm />
